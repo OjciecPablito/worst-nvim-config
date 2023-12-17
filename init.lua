18719f52c96ab6458275fe0c987387673a2c372f
OjciecPablito <pawelek710_account@tutanota.com>
2023-12-17 18:36:33 +0100

    begin custom nvim config

--- init.lua
+++ init.lua
@@ -63,7 +63,9 @@ vim.opt.rtp:prepend(lazypath)
 -- [[ Configure plugins ]]
 -- NOTE: Here is where you install your plugins.
 --  You can configure plugins using the `config` key.
---
+-- load keymaps
+require("custom.keymaps")
+require "custom.apperance"
 --  You can also configure plugins after the setup call,
 --    as they will be available in your neovim runtime.
 require('lazy').setup({
@@ -94,7 +96,75 @@ require('lazy').setup({
       'folke/neodev.nvim',
     },
   },
+  { 'gen740/SmoothCursor.nvim',
+    config = function()
+      require('smoothcursor').setup(
+        {
+            type = "exp",           -- Cursor movement calculation method, choose "default", "exp" (exponential) or "matrix".
 
+            cursor = "",              -- Cursor shape (requires Nerd Font). Disabled in fancy mode.
+            texthl = "SmoothCursor",   -- Highlight group. Default is { bg = nil, fg = "#FFD400" }. Disabled in fancy mode.
+            linehl = nil,              -- Highlights the line under the cursor, similar to 'cursorline'. "CursorLine" is recommended. Disabled in fancy mode.
+
+          fancy = {
+            enable = false,        -- enable fancy mode
+            head = { cursor = "▷", texthl = "SmoothCursor", linehl = nil }, -- false to disable fancy head
+            body = {
+              { cursor = "", texthl = "SmoothCursorRed" },
+              { cursor = "", texthl = "SmoothCursorOrange" },
+              { cursor = "●", texthl = "SmoothCursorYellow" },
+              { cursor = "●", texthl = "SmoothCursorGreen" },
+              { cursor = "•", texthl = "SmoothCursorAqua" },
+              { cursor = ".", texthl = "SmoothCursorBlue" },
+              { cursor = ".", texthl = "SmoothCursorPurple" },
+            },
+            tail = { cursor = nil, texthl = "SmoothCursor" } -- false to disable fancy tail
+          },
+
+          matrix = {  -- Loaded when 'type' is set to "matrix"
+            head = {
+              -- Picks a random character from this list for the cursor text
+              cursor = require('smoothcursor.matrix_chars'),
+              -- Picks a random highlight from this list for the cursor text
+              texthl = {
+                  'SmoothCursor',
+              },
+              linehl = nil,  -- No line highlight for the head
+            },
+            body = {
+              length = 6,  -- Specifies the length of the cursor body
+              -- Picks a random character from this list for the cursor body text
+              cursor = require('smoothcursor.matrix_chars'),
+              -- Picks a random highlight from this list for each segment of the cursor body
+              texthl = {
+                  'SmoothCursorGreen',
+              },
+            },
+            tail = {
+                -- Picks a random character from this list for the cursor tail (if any)
+                cursor = nil,
+                -- Picks a random highlight from this list for the cursor tail
+                texthl = {
+                    'SmoothCursor',
+                },
+            },
+            unstop = false,  -- Determines if the cursor should stop or not (false means it will stop)
+        },
+
+        autostart = true,           -- Automatically start SmoothCursor
+        always_redraw = true,       -- Redraw the screen on each update
+        flyin_effect = nil,         -- Choose "bottom" or "top" for flying effect
+        speed = 25,                 -- Max speed is 100 to stick with your current position
+        intervals = 35,             -- Update intervals in milliseconds
+        priority = 10,              -- Set marker priority
+        timeout = 3000,             -- Timeout for animations in milliseconds
+        threshold = 3,              -- Animate only if cursor moves more than this many lines
+        disable_float_win = false,  -- Disable in floating windows
+        enabled_filetypes = nil,    -- Enable only for specific file types, e.g., { "lua", "vim" }
+        disabled_filetypes = nil,   -- Disable for these file types, ignored if enabled_filetypes is set. e.g., { "TelescopePrompt", "NvimTree" }
+        })
+    end
+  },
   {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
@@ -216,6 +286,34 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+
+    config = function()
+      require("nvim-tree").setup {
+        sort = {
+          sorter = "case_sensitive"
+        },
+        view = {
+          width = 30
+        },
+        renderer = {
+          group_empty = true
+        },
+        filters = {
+          dotfiles = true
+        },
+      }
+    end,
+    vim.keymap.set("n", "<leader>f", "NvimTreeFocus", {desc="NvimTreeFocus"}),
+    vim.keymap.set("n", "tt", "NvimTreeToggle")
+  }
+
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
