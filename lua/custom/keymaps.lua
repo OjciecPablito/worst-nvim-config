0eea46545a0a10642e8e2aceb913a01bde1697d5
OjciecPablito <pawelek710_account@tutanota.com>
2024-03-15 16:55:05 +0100

    correct keymaps(3rd time)

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,25 +1,23 @@
 -- @param keymap /w first argument is keymap
 -- @param origin_key /w second argument
--- @param desc /w third argument is optional it provides description
+-- @param description /w third argument is optional it provides description
 function nmap(keymap, origin_key, description)
-    if desc then
+    if description then
 
-        vim.keymap.set("n", keymap .. "<CR>", origin_key, {silent=true, desc=description})
+        vim.keymap.set("n", keymap, origin_key .. "<CR>", {silent=true, desc=description})
     else
-        vim.keymap.set("n", keymap .. "<CR>", origin_key, {silent=true})
+        vim.keymap.set("n", keymap, origin_key .. "<CR>", {silent=true})
     end
 end
-vim.keymap.set("n", "<leader>v<CR>", function ()
+vim.keymap.set("n", "<leader>v", function ()
     vim.cmd("vsplit")
     local win = vim.api.nvim_get_current_win()
     local buf = vim.api.nvim_create_buf(true, true)
     vim.api.nvim_win_set_buf(win, buf)
 end,
-{silent=true, desc="split window vertically"}
-)
-nmap(";", ":")
-nmap("<leader>f", ":NvimTreeFocus", "NvimTree focus")
-nmap("tt", ":NvimeTreeToggle")
+{silent=true, desc="split window vertically"})
+vim.keymap.set("n", ";", ":")
+nmap("<leader>f", ":NvimTreeFocus", "NvimTreeFocus")
+nmap("tt", ":NvimTreeToggle")
 -- vim.keymap.set("n", ";", ":")
 -- vim.keymap.set("n", "<C-f>", ":NvimTreeToggle<CR>")
-
